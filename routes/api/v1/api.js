af9eb604f1d7c1bc25bfeef7644f8995eeb0ea0b
Nathan Bland <nathan.bland@gmail.com>
2015-11-05 15:42:29 +0000

    Adjusting file names for clarity

--- routes/api/v1/api.js
+++ routes/api/v1/api.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var router = module.exports = express.Router()
 
-var Auth = require('./authentication')
-var resources = require('./resources')
+var Auth = require('./authentication/auth')
+var resources = require('./resources/resources')
 
 router.use('/auth', Auth)
 router.use('/content', resources)
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 
 var router = module.exports = express.Router()
 
-var api = require('./api/v1')
+var api = require('./api/v1/api')
 
 router.use('/api/v1', api)
 
