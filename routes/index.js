cc4e7e0e7d15c208b71b0e871eff61f271d7f92d
Nathan Bland <nathan.bland@gmail.com>
2015-11-06 16:03:42 +0000

    minor tweaks

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 
 var router = module.exports = express.Router()
 
-router.use('/api', require('./api'))
+router.use('/api', require('./api/api'))
 
 router.get('/', function(req, res, next) {
     return res.status(200).json({
