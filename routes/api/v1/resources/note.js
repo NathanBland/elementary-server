44b3217ea1ff2ba78d863607f0625647204f773d
Joel Gimbel <jgimbel@live.com>
2015-11-05 21:43:28 -0500

    changing to promises

--- routes/api/v1/resources/note.js
+++ routes/api/v1/resources/note.js
@@ -5,80 +5,67 @@ var Note = require('../../../../models/Note')
 router.route('/') // this is /api/v1/note
     .get(function(req, res, next) {
         req.user.getNotes()
-            .sort('updated')
-            .exec(function(err, notes) {
-                if (err) {
-                    return res.status(400).json({
-                        'error': 'Internal Server Error'
-                    })
-                }
-                return res.status(200).json(notes)
-            })
+          .sort('updated')
+          .exec()
+          .then(
+              notes => res.status(200).json(notes),
+              err => res.status(400).json({'error': 'Internal Server Error', err })
+           );
     })
     .post(function(req, res, next) {
-        var note = new Note()
         var alias = req.user.username + '-' + new Date().toISOString()
-        note.set({
+        new Note({
             title: req.body.title || 'Note on ' + new Date(),
             content: req.body.content,
             alias: alias,
             user_id: req.user.user._id
         })
-        note.save(function(err) {
-            if (err) {
-                return res.status(400).json({
-                    'error': 'Internal Server Error'
-                })
-            }
-            else {
-                return res.status(201).json(note)
-            }
-        })
+        .save()
+        .then(
+            note => res.status(201).json(note),
+            err => res.status(400).json({ 'error': 'Internal Server Error', err })
+        );
+        
     })
     
 router.route('/:alias')
     .get(function(req, res, next) {
         Note.findOne({
-                alias: req.params.alias,
-                user_id: req.user.user._id
-            },
-            '-_id',
-            function(err, note) {
-                if (err) {
-                    return res.status(500).json({
-                        'error': 'Internal Server error'
-                    })
-                }
-                return res.status(200).json(note)
-            })
+            alias: req.params.alias,
+            user_id: req.user.user._id
+        })
+        .sort('-_id')
+        .then(
+            note => res.status(200).json(note),
+            err => res.status(400).json({ 'error': 'Internal Server Error', err })
+        );
     })
     .put(function(req, res, next) {
         var query = { alias: req.params.alias, user_id: req.user.user._id }
         var obj = {updated: new Date()}
-        if (req.body.title) {
+        if (req.body.title) 
           obj.title = req.body.title
-        }
-        if (req.body.content) {
+          
+        if (req.body.content)
           obj.content = req.body.content
-        }
-        Note.findOneAndUpdate(query, obj,
-         function (err, note) {
-           if (err) {
-             return res.status(500).json({
-               'error': 'Internal Server error'
-             })
-           }
-           return res.status(200).json(note)
-         })
+        
+        Note
+          .findOneAndUpdate(query, obj)
+          .exec()
+          .then(
+            note => res.status(200).json(note),
+            err => res.status(400).json({ 'error': 'Internal Server Error', err })
+          );
     })
     .delete(function(req, res, next) {
-        Note.findOneAndRemove({alias: req.params.alias, user_id: req.user.user_id},
-          function (err, result) {
-            if (err) {
-              return res.status(500).json({
-                'error': 'Internal Server error'
-              })
-            }
-            return res.status(204).json(result)
+        Note
+          .findOneAndRemove({
+              alias: req.params.alias, 
+              user_id: req.user.user_id
           })
+          .exec()
+          .then(
+            note => res.status(204).json(note),
+            err => res.status(500).json({ 'error': 'Internal Server Error', err })
+          );
     })
