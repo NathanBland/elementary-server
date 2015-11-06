559a414f76153d00921850fbba4129e7c0a3db44
Joel Gimbel <jgimbel@live.com>
2015-11-05 21:32:06 -0500

    changing api router structure

---
+++ routes/api/index.js
@@ -0,0 +1,5 @@
+var express = require('express')
+
+var router = module.exports = express.Router()
+
+router.use('/v1', require('./v1'))
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ var express = require('express')
 
 var router = module.exports = express.Router()
 
-var api = require('./api/v1/api')
-
-router.use('/api/v1', api)
+router.use('/api', require('./api'))
 
 router.get('/', function(req, res, next) {
     return res.status(200).json({
